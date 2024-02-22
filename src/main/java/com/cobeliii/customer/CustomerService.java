7016c2e64837993396077b32a0535390160d0c28
Newton Jorge <75003439+cobeliii@users.noreply.github.com>
2024-02-21 19:48:30 -0600

    Updated CustomerService.java class.

--- src/main/java/com/cobeliii/customer/CustomerService.java
+++ src/main/java/com/cobeliii/customer/CustomerService.java
@@ -1,7 +1,23 @@
 package com.cobeliii.customer;
 
+import com.cobeliii.exception.ResourceNotFound;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class CustomerService {
+    private final CustomerDao customerDao;
+
+    public CustomerService(CustomerDao customerDao) {
+        this.customerDao = customerDao;
+    }
+
+    public List<Customer> getAllCustomers(){
+        return customerDao.selectAllCustomers();
+    }
+
+    public Customer getCustomer(Integer id){
+        return customerDao.selectCustomerById(id).orElseThrow(()-> new ResourceNotFound("customer with id [%s] not found".formatted(id)));
+    }
 }
