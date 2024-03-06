577ea62827bb7398823983a35d804d2df5468c9e
Newton Jorge <75003439+cobeliii@users.noreply.github.com>
2024-03-05 20:31:43 -0600

    Added some changes

--- src/test/java/com/cobeliii/journey/CustomerIntegrationTest.java
+++ src/test/java/com/cobeliii/journey/CustomerIntegrationTest.java
@@ -1,9 +1,17 @@
 package com.cobeliii.journey;
 
+import com.cobeliii.customer.CustomerRegistrationRequest;
+import com.github.javafaker.Faker;
+import com.github.javafaker.Name;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Random;
+import java.util.UUID;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class CustomerIntegrationTest {
@@ -11,11 +19,27 @@ public class CustomerIntegrationTest {
     @Autowired
     private WebTestClient webTestClient;
 
+    private static final Random RANDOM= new Random();
+
     @Test
     void itShouldRegisterACustomer() {
         //Create a registration request
+        Faker faker = new Faker();
+        Name fakerName = faker.name();
+        String name = fakerName.fullName();
+        String email = fakerName.lastName() + UUID.randomUUID() + "@gmail.com";
+        int age = RANDOM.nextInt(1, 100);
 
+        CustomerRegistrationRequest request = new CustomerRegistrationRequest(name, email, age);
         //Send a post request
+        webTestClient.post()
+                .uri("/api/v1/customers")
+                .accept(MediaType.APPLICATION_JSON)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(request), CustomerIntegrationTest.class)
+                .exchange()
+                .expectStatus()
+                .isOk();
         //get All customers
         //make sure that customer is present
     }
